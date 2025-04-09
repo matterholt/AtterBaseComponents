5083432402f1059c06baf8fafea9d7947fbb3626
MAtterholt <matterholt.dev@gmail.com>
2025-04-08 23:03:15 -0400

    copied base comps

--- base_components/index.ts
+++ base_components/index.ts
@@ -1,6 +1,10 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
 
-import MyComponent from './src/MyComponent.astro';
+// import Text from "./src/Text.astro";
+// import List from "./src/List.astro";
+// import LinkTo from "./src/LinkTo.astro";
 
-export default MyComponent;
+export { default as Text } from "./src/Text.astro";
+export { default as List } from "./src/List.astro";
+export { default as LinkTo } from "./src/LinkTo.astro";
